5d4fd7188aa6788594fbf1b567328e263e780583
Shunyu Wang <w_shunyu@live.concordia.ca>
2018-10-02 02:05:19 -0400

    renaming classes and add some code base

--- HttpClient/src/main/java/ca/concordia/httpClient/lib/ClientHttpRequest.java
+++ HttpClient/src/main/java/ca/concordia/httpClient/lib/ClientHttpRequest.java
@@ -1,5 +1,20 @@
 package ca.concordia.httpClient.lib;
 
-public interface ClientHttpRequest {
+import java.io.Serializable;
+
+public abstract class ClientHttpRequest implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Enum<HttpMethod> method;
+	
+	private String URI;
+	
+	private String version = "HTTP/1.0";
+	
+	private String host;
 
 }
